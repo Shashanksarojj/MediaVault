f4af1549f090ad99d7cac5b24fbc1ed4b52d11b6
Shashanksarojj <shashankdw1704@gmail.com>
2024-06-07 18:37:29 +0530

    added aws-sdk

--- Backend/routes/media.js
+++ Backend/routes/media.js
@@ -1,19 +1,46 @@
+// Configure Multer storage for server storage
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + '-' + file.originalname);
+//     },
+// });
+
 const express = require('express');
 const multer = require('multer');
+const { S3Client, DeleteObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { Upload } = require('@aws-sdk/lib-storage');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const multerS3 = require('multer-s3');
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const { verifyToken } = require('../middleware/authMiddleware');
 const Media = require('../models/Media'); // Assuming you have a Media model
 const router = express.Router();
 
-// Configure Multer storage
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+// Configure AWS SDK
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-    },
+});
+
+const myBucket = process.env.AWS_BUCKET_NAME;
+
+// Configure Multer storage for S3
+const storage = multerS3({
+    s3: s3Client,
+    bucket: myBucket,
+    acl: "bucket-owner-full-control",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+        cb(null, Date.now().toString() + '-' + file.originalname);
+    }
 });
 
 const upload = multer({ storage: storage });
@@ -24,14 +51,22 @@ router.post('/uploadImage', verifyToken, upload.single('image'), async (req, res
         if (!req.file) {
             return res.status(400).json({ message: "No file uploaded." });
         }
+        // Generate a pre-signed URL valid for 15 hours
+        const params = {
+            Bucket: myBucket,
+            Key: req.file.key,
+            Expires: 60 * 15, // 15 min in seconds
+        };
+        const url = await getSignedUrl(s3Client, new GetObjectCommand(params));
+
         const newMedia = new Media({
             userId: req.user.user_id,
-            filePath: req.file.path,
-            fileName: req.file.filename,
+            filePath: req.file.location,
+            fileName: req.file.key,
             mimeType: req.file.mimetype,
         });
         await newMedia.save();
-        res.status(201).json({ filePath: req.file.path });
+        res.status(201).json({ "Imageurl15min": url });
     } catch (error) {
         res.status(500).json({ message: "Error uploading media.", error });
     }
@@ -43,15 +78,23 @@ router.post('/uploadBulkImage', verifyToken, upload.array('images', 4), async (r
         if (!req.files || req.files.length === 0) {
             return res.status(400).json({ message: "No files uploaded." });
         }
+
+        const params = {
+            Bucket: myBucket,
+            Key: req.files.key,
+            Expires: 60 * 15, // 15 hours in seconds
+        };
+        const url = await getSignedUrl(s3Client, new GetObjectCommand(params));
+
         const mediaFiles = req.files.map(file => ({
             userId: req.user.user_id,
-            filePath: file.path,
-            fileName: file.filename,
+            filePath: file.location,
+            fileName: file.key,
             mimeType: file.mimetype,
         }));
 
         await Media.insertMany(mediaFiles);
-        res.status(201).json({ message: "Images uploaded successfully.", files: req.files });
+        res.status(201).json({ message: "Images uploaded successfully.", "signed-url": url });
     } catch (error) {
         res.status(500).json({ message: "Error uploading images.", error });
     }
@@ -69,12 +112,12 @@ router.post('/uploadVideo', verifyToken, upload.single('video'), async (req, res
         }
         const newMedia = new Media({
             userId: req.user.user_id,
-            filePath: req.file.path,
-            fileName: req.file.filename,
+            filePath: req.file.location,
+            fileName: req.file.key,
             mimeType: req.file.mimetype,
         });
         await newMedia.save();
-        res.status(201).json({ filePath: req.file.path });
+        res.status(201).json({ filePath: req.file.location });
     } catch (error) {
         res.status(500).json({ message: "Error uploading video.", error });
     }
@@ -84,7 +127,19 @@ router.post('/uploadVideo', verifyToken, upload.single('video'), async (req, res
 router.get('/get-media', verifyToken, async (req, res) => {
     try {
         const mediaFiles = await Media.find({ userId: req.user.user_id });
-        res.status(200).json(mediaFiles);
+        // Generate signed URLs for each media file
+        const signedMediaFiles = await Promise.all(mediaFiles.map(async (media) => {
+            const params = {
+                Bucket: myBucket,
+                Key: media.fileName,
+                Expires: 60 * 15, // 24 hours in seconds (adjust as needed)
+            };
+            const url = await getSignedUrl(s3Client, new GetObjectCommand(params));
+            return { ...media.toObject(), ImageUrl: url };
+        }));
+
+
+        res.status(200).json(signedMediaFiles);
     } catch (error) {
         res.status(500).json({ message: "Error retrieving media files.", error });
     }
@@ -98,8 +153,13 @@ router.get('/media/:id', verifyToken, async (req, res) => {
             return res.status(404).json({ message: "Media not found or unauthorized." });
         }
 
-        const filePath = path.join(__dirname, '..', media.filePath);
-        res.sendFile(filePath);
+        // Redirect to the S3 file URL
+        const params = {
+            Bucket: myBucket,
+            Key: media.fileName,
+        };
+        const url = await getSignedUrl(s3Client, new GetObjectCommand(params));
+        res.redirect(url);
     } catch (error) {
         res.status(500).json({ message: "Error retrieving media file.", error });
     }
@@ -112,18 +172,23 @@ router.delete('/del-media/:id', verifyToken, async (req, res) => {
         if (!media || media.userId.toString() !== req.user.user_id) {
             return res.status(404).json({ message: "Media not found or unauthorized." });
         }
-        const filePath = path.join(__dirname, '..', media.filePath);
-        fs.unlink(filePath, async (err) => {
-            if (err) {
-                return res.status(500).json({ message: "Error deleting file.", err });
-            }
-            await Media.deleteOne({ _id: req.params.id });
-            res.status(200).json({ message: "Media deleted successfully." });
-        });
+
+        // console.log("Media Found!")
+        const params = {
+            Bucket: myBucket,
+            Key: media.fileName
+        };
+        // console.log("Params: " + params);
+        const command = new DeleteObjectCommand(params);
+        // console.log("Command Created!")
+        await s3Client.send(command);
+        // console.log("Command Send!")
+        await Media.deleteOne({ _id: req.params.id });
+        res.status(200).json({ message: "Media deleted successfully." });
     } catch (error) {
+        console.log("Error deleting!")
         res.status(500).json({ message: "Error deleting media.", error });
     }
 });
-
 
 module.exports = router;
